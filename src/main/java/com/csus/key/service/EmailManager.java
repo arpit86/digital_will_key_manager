0cb6605759ba06aac0ff1925789fef7aa54a6f9f
ShwetaS <shweta@shweta>
2018-12-03 22:48:05 -0800

    first commit

---
+++ src/main/java/com/csus/key/service/EmailManager.java
@@ -0,0 +1,122 @@
+package com.csus.key.service;
+
+import java.io.File;
+import java.util.Properties;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+public class EmailManager {
+	
+	private Session getSession() {
+		Properties property = new Properties();
+		property.put("mail.smtp.auth", "true");
+		property.put("mail.smtp.port", "587");
+		property.put("mail.smtp.host", "smtp.gmail.com");
+		property.put("mail.smtp.starttls.enable", "true");
+		property.put("mail.smtp.user", "fall2018.blockchain@gmail.com");
+		property.put("mail.smtp.password", "Fall2018");
+		
+		Session session = Session.getInstance(property);
+		return session;
+	}
+	
+	private void sendMessage(MimeMessage message, Session session) {
+		try {
+			Transport transport = session.getTransport("smtp");
+			transport.connect(null,"fall2018.blockchain@gmail.com","Fall2018");
+			transport.sendMessage(message, message.getAllRecipients());
+			transport.close();
+		} catch (MessagingException ex) {
+			System.out.println("EmailService:sendMessage:: MessagingException: " + ex.getMessage());
+		}
+	}
+	
+	public void sendEmailContainingThePrivateKey(byte[] data, String email) {
+		//String receiver = email;
+		String receiver = "fall2018.blockchain@gmail.com";
+		String from = "fall2018.blockchain@gmail.com";
+
+		// Get the session object
+		Session session = getSession();
+
+		// Save the encoded Private key to a file in format <User_email>_priv.txt
+		File file = new File("KeyPair/privateKey_" + email);
+		try {
+			// compose the message
+			MimeMessage message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(from));
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(receiver));
+			message.setFrom(new InternetAddress(from));
+			message.setSubject("Thank you for using Digital Will Key Manager, "+ email);
+			message.setText("Please save the attached file securely on your computer.\n"
+					+ "IMPORTANT NOTE:  The will uploaded can only be read with help of this file.\n"
+					+ "ONCE THIS FILE IS CORRUPTED, THE WILL NO LONGER BE AVAILABLE OR RETRIEVED.");
+			// Attaching the file containing encoded Private key
+			MimeBodyPart mBodyPart = new MimeBodyPart();
+			Multipart multipart = new MimeMultipart();
+			mBodyPart = new MimeBodyPart();
+			DataSource source = new FileDataSource(file);
+			mBodyPart.setDataHandler(new DataHandler(source));
+			mBodyPart.setFileName("privateKey");
+			multipart.addBodyPart(mBodyPart);
+			message.setContent(multipart);
+			
+			// Send message
+			sendMessage(message, session);
+			System.out.println("The email sent was:\n" + message.toString());
+		} catch (MessagingException mex) {
+			mex.printStackTrace();
+		}
+	}
+	
+	public void sendEmailAuthorizeUserToRegister(byte[] privateKey, String userEmail, int will_id) {
+		//String receiver = userEmail;
+		String receiver = "fall2018.blockchain@gmail.com";
+		String from = "fall2018.blockchain@gmail.com";
+
+		System.out.println("Before the session");
+
+		// Get the session object
+		Session session = getSession();
+
+		// Save the encoded Private key to a file in format <User_email>_priv.txt
+		File file = new File("KeyPair/privateKey_" + userEmail);
+
+		// compose the message
+		try {
+			MimeMessage message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(from));
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(receiver));
+			message.setSubject("Thank you for using Digital Will Key Manager, " + userEmail);
+			message.setText("Please save the attached file securely on your computer.\n"
+					+ "IMPORTANT NOTE:  Please register to the Digital Vault Application.\n"
+					+ "YOU HAVE BEEN AUTHORIZED TO VIEW THE WILL #" + will_id);
+			// Attaching the file containing encoded Private key
+			MimeBodyPart mBodyPart = new MimeBodyPart();
+			Multipart multipart = new MimeMultipart();
+			mBodyPart = new MimeBodyPart();
+			DataSource source = new FileDataSource(file);
+			mBodyPart.setDataHandler(new DataHandler(source));
+			mBodyPart.setFileName("privateKey");
+			multipart.addBodyPart(mBodyPart);
+			message.setContent(multipart);
+
+			// Send message
+			sendMessage(message, session);
+			System.out.println("The email sent was:\n" + message.toString());
+		} catch (MessagingException mex) {
+			mex.printStackTrace();
+		}
+	}
+}
---
+++ src/main/java/com/csus/key/service/KeyManager.java
@@ -0,0 +1,123 @@
+package com.csus.key.service;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.security.KeyFactory;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+
+import javax.crypto.spec.SecretKeySpec;
+
+public class KeyManager {
+	
+	private EmailManager emailManager = null;
+	
+	/*
+	 *  This function generate private-public key pair using RSA algorithm for Registered User.
+	 */
+	public byte[] generateKeyPair(String userEmail) throws NoSuchAlgorithmException {
+		
+		KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+        keyGen.initialize(512);
+        byte[] publicKey = keyGen.genKeyPair().getPublic().getEncoded();
+        byte[] privateKey = keyGen.genKeyPair().getPrivate().getEncoded();
+        //user.setUser_publicKey(publicKey);
+        
+        System.out.println("Public key is saved in database and the Private key is emailed to user.");
+        // The public-private key is saved to KeyPair folder: <keyType>_<userEmail>
+        writeToFile("KeyPair/publicKey_" + userEmail, publicKey);
+        writeToFile("KeyPair/privateKey_" + userEmail, privateKey);
+        
+        //Send an email to user with private key to the user email
+        emailManager = new EmailManager();
+        emailManager.sendEmailContainingThePrivateKey(privateKey, userEmail);
+        
+        return publicKey;
+	}
+	
+	/*
+	 *  This function generate private-public key pair using RSA algorithm for Authorized User.
+	 */
+	public byte[] generateKeyPairForAuthorizedUser(String userEmail, int willId) throws NoSuchAlgorithmException {
+		KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+		keyGen.initialize(512);
+		byte[] publicKey = keyGen.genKeyPair().getPublic().getEncoded();
+		byte[] privateKey = keyGen.genKeyPair().getPrivate().getEncoded();
+		
+		System.out.println("Public key is saved in database and the Private key is emailed to user.");
+		// The public-private key is saved to KeyPair folder: <keyType>_<userEmail>
+		writeToFile("KeyPair/publicKey_" + userEmail, publicKey);
+		writeToFile("KeyPair/privateKey_" + userEmail, privateKey);
+			
+		// Send an email to user with private key to the user email
+		emailManager = new EmailManager();
+		emailManager.sendEmailAuthorizeUserToRegister(privateKey, userEmail, willId);
+		return publicKey;
+	}
+	
+	/*
+	 *  This function generates session key using AES algorithm for the Will owner.
+	 */
+	public void generateSecretKey(String userEmail) {
+    	SecureRandom rnd = new SecureRandom();
+        byte [] key = new byte [16];
+        rnd.nextBytes(key);
+        SecretKeySpec secretKey = new SecretKeySpec(key, "AES");
+        writeToFile("SecretKey/symKey_"+ userEmail, secretKey.getEncoded());
+    }
+	
+	/*
+	 * Obtain the private key from the file uploaded by the user.
+	 */
+	public PrivateKey getPrivate(String userEmail) throws Exception {
+        byte[] keyBytes = Files.readAllBytes(new File("KeyPair/privateKey_" + userEmail).toPath());
+        PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        return keyFactory.generatePrivate(spec);
+    }
+
+	/*
+	 * Obtain the public key associated with the email of the user.
+	 */
+    public PublicKey getPublic(String userEmail) throws Exception {
+        byte[] keyBytes = Files.readAllBytes(new File("KeyPair/publicKey_" + userEmail).toPath());
+        X509EncodedKeySpec spec = new X509EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        return keyFactory.generatePublic(spec);
+    }
+	
+    /*
+	 * Obtain the session key associated with the email of the user.
+	 */
+    public SecretKeySpec getSecretKey(String userEmail) throws IOException{
+        byte[] keyBytes = Files.readAllBytes(new File("SecretKey/symKey_"+ userEmail).toPath());
+        return new SecretKeySpec(keyBytes, "AES");
+    }
+	
+	/*
+	 *  This function writes the byte[] data to the file path provided.
+	 */
+	private void writeToFile(String path, byte[] key) {
+		try {
+			File f = new File(path);
+			f.getParentFile().mkdirs();
+			FileOutputStream fos = new FileOutputStream(f);
+			fos.write(key);
+	        fos.flush();
+	        fos.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
